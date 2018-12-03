cb11bd1891f4d7c3c69d13c70da2ee9c18339711
Szymon Wojnar <wojnar48@gmail.com>
2018-12-03 13:14:59 -0500

    Add styling to the FilterTable component

--- components/FilterTable/index.js
+++ components/FilterTable/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 
+
+const renderTags = (tokens) => (
+  <div className='tags is-centered'>
+    {tokens.map(token => <span className='tag is-warning'>{token}</span>)}
+  </div>
+);
+
 const FilterTable = ({ filters }) => {
   return (
     <table className='table is-hoverable is-fullwidth'>
       <thead>
         <tr>
-          <th className='has-text-centered'>Name</th>
-          <th className='has-text-centered'>Filter Terms</th>
-          <th className='has-text-centered'>Created</th>
+          <th className='has-text-centered has-text-grey'>Name</th>
+          <th className='has-text-centered has-text-grey'>Filter Terms</th>
+          <th className='has-text-centered has-text-grey'>Created</th>
         </tr>
       </thead>
       <tbody>
         {filters.map(filter => (
           <tr>
             <td className='has-text-centered'>{filter.name}</td>
-            <td className='has-text-centered'>{filter.filterTerms.join(', ')}</td>
+            <td className='has-text-centered'>{renderTags(filter.filterTerms)}</td>
             <td className='has-text-centered'>{filter.createdAt}</td>
           </tr>)
         )}
