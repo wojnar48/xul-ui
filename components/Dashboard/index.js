77a2fe309164e3dae8c9b4741b03839d87519444
Szymon Wojnar <wojnar48@gmail.com>
2018-12-07 21:31:15 -0500

    Modify the Dashboard component to render AuthFormContainer if user is not logged in

--- components/Dashboard/index.js
+++ components/Dashboard/index.js
@@ -3,7 +3,8 @@ import Link from 'next/link';
 import { Query } from 'react-apollo';
 
 import FilterTable from './FilterTable';
-import RequiresAuth from '../RequiresAuth';
+import User from '../User';
+import AuthFormContainer from '../AuthForm';
 import { ALL_FILTERS_QUERY } from '../../graphql';
 
 
@@ -12,38 +13,45 @@ class Dashboard extends Component {
 
   render() {
     return (
-      <RequiresAuth>
-        <section className='section dashboard-section'>
-          <div className='container dashboard-container'>
-            <div className='columns'>
-              <div className='column'>
-                <nav className='level'>
-                  <div className='level-left'>
-                    <h3 className='title has-text-grey'>Filters</h3>
+      <User>
+        {({ data }) => {
+          // Show the auth form if no current user is present
+          if (!data.me) return <AuthFormContainer />;
+
+          return (
+            <section className='section dashboard-section'>
+            <div className='container dashboard-container'>
+              <div className='columns'>
+                <div className='column'>
+                  <nav className='level'>
+                    <div className='level-left'>
+                      <h3 className='title has-text-grey'>Filters</h3>
+                    </div>
+                    <div className='level-right'>
+                      <button className='button is-primary'>
+                        <Link href='/create-filter'>
+                          <a>Add Filter</a>
+                        </Link>
+                      </button>
+                    </div>
+                  </nav>
+                  <div className='box'>
+                    <Query query={ALL_FILTERS_QUERY}>
+                      {({ data, loading, error }) => {
+                        if (loading) return <p>Loading...</p>;
+                        if (error) return <p>{`Error: ${error.message}`}</p>
+                        
+                        return <FilterTable filters={data.filters} />;
+                      }}
+                    </Query>
                   </div>
-                  <div className='level-right'>
-                    <button className='button is-primary'>
-                      <Link href='/create-filter'>
-                        <a>Add Filter</a>
-                      </Link>
-                    </button>
-                  </div>
-                </nav>
-                <div className='box'>
-                  <Query query={ALL_FILTERS_QUERY}>
-                    {({ data, loading, error }) => {
-                      if (loading) return <p>Loading...</p>;
-                      if (error) return <p>{`Error: ${error.message}`}</p>
-                      
-                      return <FilterTable filters={data.filters} />;
-                    }}
-                  </Query>
                 </div>
               </div>
             </div>
-          </div>
-        </section>
-      </RequiresAuth>
+          </section>
+          )
+        }}
+      </User>
     );
   }
 }
