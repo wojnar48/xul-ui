9c85f04fd03993da3d8ad1f622755966433daacd
Szymon Wojnar <wojnar48@gmail.com>
2018-12-11 10:53:18 -0500

    Add getInitialProps to the topmost _app component to pass through page props

--- pages/_app.js
+++ pages/_app.js
@@ -8,15 +8,29 @@ import withData from '../lib/withData';
 // Creates a custom component that is used by Next.js to wrap individual pages.
 // See: https://nextjs.org/docs/#custom-app.
 class AppContainer extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps  = {};
+
+    // If the Component has any initial props, get them
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    // Expose the query to the user
+    pageProps.query = ctx.query;
+
+    return { pageProps };
+  }
+
   render() {
     // `this.props.apollo` is available because of the HOC `withData` generates
-    const { Component, apollo } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
         <ApolloProvider client={apollo}>
           <Page>
-            <Component />
+            <Component {...pageProps} />
           </Page>
         </ApolloProvider>
       </Container>
@@ -24,6 +38,4 @@ class AppContainer extends App {
   }
 }
 
-// Wrapping in withData makes the apollo client accessible
-// via this.props.
 export default withData(AppContainer);
